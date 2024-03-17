cd2a37f04527d641628ebba5866b40a1412d497d
mhcho <1jmhcho@gmail.com>
2024-03-17 23:02:36 +0900

    from JSON 커밋

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,26 @@
-import 'package:http/http.dart' as http; // namespace 지정
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_model.dart'; // namespace 지정
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
 
     // Future<Response> : get 함수는 미래에 완료됨
     final response = await http.get(url); // await는 비동기 함수 내에서만 사용 가능함
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      // 텍스트로 된 응답 body를 JSON으로 디코딩
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
